b40af95dae57d64ce58ad38959425ec656875f7c
4libutt <ali@vergelijkgroep.nl>
2020-12-02 15:55:43 +0100

    added Viewmodel class and MovieOverviewFragment logic

--- app/src/main/java/com/example/madlevel6task2/MovieOverviewFragment.kt
+++ app/src/main/java/com/example/madlevel6task2/MovieOverviewFragment.kt
@@ -6,12 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.madlevel6task2.model.MovieResponse
+import com.example.madlevel6task2.viewmodel.MovieViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_movie_overview.*
+
+const val BUNDLE_MOVIE_KEY = "bundle_movie_key"
+const val REQ_MOVIE_KEY = "req_movie_key"
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class MovieOverviewFragment : Fragment() {
+
+    private val movies = arrayListOf<MovieResponse.Movie>()
+    private val moviesAdapter = MovieAdapter(movies, ::onMovieClick)
+    private val movieViewModel: MovieViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +39,42 @@ class MovieOverviewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        activity?.title = "Popular movies"
+        initRv()
+
+        btn_submit.setOnClickListener {
+            val year = inputYear.text
+
+            if(year?.isEmpty()!!) {
+                Snackbar.make(btn_submit, R.string.failed, Snackbar.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            movieViewModel.getMovies(year.toString())
+        }
+
+        movieViewModel.movies.observe(viewLifecycleOwner, Observer {
+            movies.clear()
+            movies.addAll(it)
+            moviesAdapter.notifyDataSetChanged()
+        })
+
+        movieViewModel.errorText.observe(viewLifecycleOwner, Observer {
+            Snackbar.make(btn_submit, R.string.failed, Snackbar.LENGTH_SHORT).show()
+        })
+
+    }
+
+    private fun onMovieClick(movie: MovieResponse.Movie) {
+        setFragmentResult(REQ_MOVIE_KEY, bundleOf(Pair(BUNDLE_MOVIE_KEY, movie)))
+        findNavController().navigate(R.id.action_MovieOverviewFragment_to_MovieInfoFragment)
+    }
+
+    private fun initRv() {
+        rvMovies.apply {
+            adapter = moviesAdapter
+            layoutManager = GridLayoutManager(context, 2)
+
+        }
     }
 }
