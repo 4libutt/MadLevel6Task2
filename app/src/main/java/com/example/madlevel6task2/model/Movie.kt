a76af30f217d6a4604ddbd521ce71bde95d3ffc4
4libutt <ali@vergelijkgroep.nl>
2020-12-02 14:30:48 +0100

    added Movie model class

---
+++ app/src/main/java/com/example/madlevel6task2/model/Movie.kt
@@ -0,0 +1,27 @@
+package com.example.madlevel6task2.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+class MovieResponse() {
+
+    data class Root (
+        @SerializedName("results") val results : List<Movie>
+    )
+
+    @Parcelize
+    data class Movie (
+        @SerializedName("poster_path") val poster_path : String,
+        @SerializedName("backdrop_path") val backdrop_path : String,
+        @SerializedName("title") val title : String,
+        @SerializedName("vote_average") val vote_average : Double,
+        @SerializedName("overview") val overview : String,
+        @SerializedName("release_date") val release_date : String
+    ) : Parcelable {
+        fun getMovieImageUrl(posterPath: String): String {
+            return "https://image.tmdb.org/t/p/original%s".format(posterPath)
+        }
+    }
+
+}
